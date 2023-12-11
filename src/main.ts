30537827abc59ed9ffbb5b2c5f3ea4adf0d53c92
axunion <120321953+axunion@users.noreply.github.com>
2023-12-11 23:48:53 +0900

    Integrate loader component

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import "./style.css";
 import * as elements from "@/components/elements";
 import * as layouts from "@/components/layouts";
 import * as forms from "@/components/forms";
+import { loaderState } from "@/store";
+
+// -----------------------------------------------------------------------------
+// Custom Element
+// -----------------------------------------------------------------------------
 
 interface CustomElement extends CustomElementConstructor {
   componentName: string;
@@ -15,4 +20,20 @@ const components: CustomElement[] = [
 
 for (const component of components) {
   customElements.define(component.componentName, component);
+}
+
+// -----------------------------------------------------------------------------
+// Loader
+// -----------------------------------------------------------------------------
+
+const loaderComponent = document.querySelector("loader-component");
+
+if (loaderComponent) {
+  loaderState.addSubscriber(({ state }) => {
+    if (state) {
+      loaderComponent.classList.remove("hidden");
+    } else {
+      loaderComponent.classList.add("hidden");
+    }
+  });
 }
