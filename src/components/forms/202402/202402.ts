902113a071eeed1aa6851392c7975da707c0e784
axunion <120321953+axunion@users.noreply.github.com>
2023-12-19 00:00:05 +0900

    Change DOM from shadow to light

--- src/components/forms/202402/202402.ts
+++ src/components/forms/202402/202402.ts
@@ -1,20 +1,20 @@
 import { BaseElement } from "@/services";
 import template from "./202402.html?raw";
 import { loaderState } from "@/store";
-import { ButtonComponent, InputTextComponent } from "@/components/elements";
 
 export class Form202402Component extends BaseElement {
   static componentName = "jgxc892kn9low-ljph4q3x8w95t04ykn5z2o6ndd";
 
   constructor() {
-    super(template);
+    super(template, false);
   }
 
   connectedCallback() {
-    const $ = (s: string) => this.shadowRoot?.querySelector(s);
-    const $_ = (s: string) => this.shadowRoot?.querySelectorAll(s);
-    const inputs = ($_("input-text-component") || []) as InputTextComponent[];
-    const formButton = $("form button-component") as ButtonComponent;
+    const $ = (s: string) => this.querySelector(s);
+    const $_ = (s: string) => this.querySelectorAll(s);
+    const inputs = ($_(".input-text input") ||
+      []) as NodeListOf<HTMLInputElement>;
+    const formButton = $(".button-outlined") as HTMLButtonElement;
 
     for (const input of inputs) {
       input.addEventListener("keyup", (event) => {
