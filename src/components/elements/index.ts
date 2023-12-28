85d0c1e7933191f5ab3b71825c515e17b39df5dd
axunion <120321953+axunion@users.noreply.github.com>
2023-12-28 23:48:11 +0900

    Clean up excess components

--- src/components/elements/index.ts
+++
@@ -1,6 +0,0 @@
-export { ButtonComponent } from "./button/Button";
-export { CanvasComponent } from "./canvas/Canvas";
-export { InputFileComponent } from "./input-file/InputFile";
-export { InputTextComponent } from "./input-text/InputText";
-export { SelectComponent } from "./select/Select";
-export { SvgIcon } from "./svg-icon/SvgIcon";
