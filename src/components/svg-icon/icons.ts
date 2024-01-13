98a9f3cfbc6c160b94b6d66b9dfdbe112be01275
axunion <120321953+axunion@users.noreply.github.com>
2024-01-13 11:12:28 +0900

    Move Entry page to a different repository

--- src/components/svg-icon/icons.ts
+++
@@ -1,3 +0,0 @@
-export const icons: { [key: string]: string } = {
-  menu: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2 6H22V8H2V8Z M2 16H22V18H2V14Z"></path></svg>',
-};
