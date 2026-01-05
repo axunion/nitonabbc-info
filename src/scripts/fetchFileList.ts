61d5fa7f908a678face8e74ac917478e78ba9501
axunion <120321953+axunion@users.noreply.github.com>
2026-01-05 23:51:06 +0900

    fix: update gallery path to include Japanese characters and improve URL encoding in fetchFileList

--- src/scripts/fetchFileList.ts
+++ src/scripts/fetchFileList.ts
@@ -4,7 +4,7 @@ import type { FetchFileListResponse } from "@/types/api";
 export const fetchFileList = async (
   path: string,
 ): Promise<FetchFileListResponse> => {
-  const query = path ? `?path=${path}` : "";
+  const query = path ? `?path=${encodeURIComponent(path)}` : "";
   const res = await fetch(`${ENDPOINT_LIST}${query}`);
 
   return (await res.json()) as FetchFileListResponse;
